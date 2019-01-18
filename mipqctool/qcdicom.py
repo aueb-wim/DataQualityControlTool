3aeb65343136b90eae15b3ce998bb546b5f3ba06
iosif <iosifspart@yahoo.gr>
2019-01-18 16:18:14 +0200

    added multiprocessing capabilities in qcdicom

--- mipqctool/qcdicom.py
+++ mipqctool/qcdicom.py
@@ -1,6 +1,8 @@
 # qcdicom.py
 import datetime
 import os
+import multiprocessing as mp
+from multiprocessing import Pool
 import pydicom
 import pandas as pd
 from . import __version__
@@ -32,11 +34,39 @@ def getsubfolders(rootfolder):
     return result
 
 
+def getsubfolders2list(rootfolder):
+    """Returns dict with keys subfolders and values a list
+    of the containing dcm files in each folder"""
+    dirtree = os.walk(rootfolder)
+    subfolders = set()
+    result = []
+    for root, dirs, files in dirtree:
+        del dirs  # Not used
+        # Get all the visible subfolders
+        for name in files:
+            subfolder, filename = removeroot(os.path.join(root, name),
+                                             rootfolder)
+            if subfolder in subfolders:
+                result.append([subfolder, filename])
+            else:
+                result[subfolder] = [filename]
+    return result
+
+
 def removeroot(filepath, rootfolder):
     """Removes the root path from a given filepath."""
     subfolder = os.path.dirname(os.path.relpath(filepath, rootfolder))
     filename = os.path.basename(filepath)
     return (subfolder, filename)
+
+
+def splitdict(bigdict, n):
+    """Split a dictionary to n parts."""
+    chunksize = len(bigdict) / float(n)
+    for i in range(n):
+        start = int(round(i * chunksize))
+        end = int(round((i + 1) * chunksize))
+        yield dict(list(bigdict.items())[start:end])
 
 class DicomReport(object):
     """A class for producing a metadata report of
@@ -56,7 +86,12 @@ class DicomReport(object):
         self.rootfolder = rootfolder
         self.subfolders = getsubfolders(rootfolder)
         self.username = username
-        self.readicoms()
+        self.dataset = {'version': [__version__],
+                        'date_qc_ran': [datetime.datetime.now()],
+                        'username': [username],
+                        'dicom_folder': [os.path.abspath(rootfolder)]}
+        self.readicoms_parallel(mp.cpu_count() + 1)
+#        self.readicoms()
 
     def _set_requirements(self, df):
         mand = df[df['mandatory'] == 'Yes']
@@ -84,40 +119,57 @@ class DicomReport(object):
             data['file'] = filename
             for tag in columns:
                 # Don't tags that represent sequence
-                # Sequence tags are big strings and contain commas that corrupt the exported
-                # csv file
+                # Sequence tags are big strings and contain commas
+                # that corrupt the exported csv file
                 if not tag.endswith('Sequence'):
                     try:
                         data[tag] = [ds.data_element(tag).value]
                     except AttributeError:
-                        data[tag] = 'Error! Value not found!'
+                        data[tag] = ['Error! Value not found!']
             dicomdf = pd.DataFrame.from_dict(data)
             return dicomdf
         except pydicom.errors.InvalidDicomError:
             raise pydicom.errors.InvalidDicomError
 
+    def readicoms_parallel(self, processes):
+        """Read all the dicoms using multiprocessing."""
+        if len(self.subfolders) > processes:
+            print('dicom parallel processing with {} Processes'.format(processes))
+            slices = list(splitdict(self.subfolders, processes))
+            with Pool(processes) as p:
+                dcms = p.map(self.readicoms_chunks, slices)
+                self.dicoms = pd.concat(dcms, ignore_index=True)
+            self.dicoms.set_index(['folder', 'file'], inplace=True)
+        else:
+            print('Single core processing...')
+            self.readicoms()
+
+
     def readicoms(self):
-        dataset = {'version': [__version__],
-                   'date_qc_ran': [datetime.datetime.now()],
-                   'username': [self.username],
-                   'dicom_folder': [os.path.abspath(self.rootfolder)]}
-        self.dataset = pd.DataFrame.from_dict(dataset)
-        for folder in self.subfolders:
-            for dicom in self.subfolders[folder]:
+        self.dicoms = self.readicoms_chunks(self.subfolders)
+        self.dicoms.set_index(['folder', 'file'], inplace=True)
+
+
+    def readicoms_chunks(self, filesdict):
+        """Read the given dicoms and return a df."""
+        dcms = []
+        for folder in filesdict:
+#            dcms = []  # dicoms df from the same folder
+            for dicom in filesdict[folder]:
                 try:
-                    with open('qctool_dicom_processed.log', 'a') as f:
+                    with open('qctool_dicom_files_read.log', 'a') as f:
                         f.write(folder + ',' + dicom + '\n')
-                    dicomdf = self._read_dicom(dicom, folder)
-                    self.dicoms = pd.concat([self.dicoms, dicomdf],
-                                            ignore_index=True)
+                    dcms.append(self._read_dicom(dicom, folder))
                 except pydicom.errors.InvalidDicomError:
                     continue
-        self.dicoms.set_index(['folder', 'file'], inplace=True)
+        return pd.concat(dcms, ignore_index=True)
 
     def export2xls(self, filepath):
         """Export the report in excel file"""
         writer = pd.ExcelWriter(filepath)
-        self.dataset.to_excel(writer, sheet_name='general_info',
-                              index=False)
+        dataset_df = pd.DataFrame.from_dict(self.dataset)
+        dataset_df.to_excel(writer, sheet_name='general_info',
+                            index=False)
         self.dicoms.to_excel(writer, sheet_name='dicom_metadata')
+        self.dicoms.to_csv(filepath[:-4] + '.csv')
         writer.save()
