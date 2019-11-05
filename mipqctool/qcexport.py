af1bfd2e884ac1706682e5293c2863759bbbd8d9
iosif <iosifspart@yahoo.gr>
2019-11-05 17:17:48 +0200

    removed latex2pdf support completly

--- mipqctool/qcexport.py
+++ mipqctool/qcexport.py
@@ -2,42 +2,14 @@
 """ Module with helper functions for exporting the statistical reports
 of the MIP local Quality Control Tool
 """
-'''
-import os.path
-from pylatex import Document, Section, Foot, MiniPage, \
-    LongTabu, LineBreak, NewPage, Tabularx, TextColor, simple_page_number
-from pylatex.utils import NoEscape
-'''
 
+import os
 from jinja2 import Environment, FileSystemLoader
 from weasyprint import HTML
 
 from .config import LOGGER
 
-
-######### Functions for exporting in pdf using LaTex #########
-
-def fill_stat(doc, df):
-    """Helper fucntion returns a LaTex Table from pandas df.
-
-    Arguments:
-    :param doc: pylatex document class
-    :parma df: pandas dataframe
-    """
-    with doc.create(LongTabu('X[2l] X[r]',
-                             row_height=1.5)) as data_table:
-        data_table.add_hline()
-        data = df.reset_index(level=0)
-        data = data.values.tolist()
-        totalrows = len(data)
-        for i in range(totalrows):
-            if (i % 2) == 0:
-                data_table.add_row(data[i], color='lightgray')
-            else:
-                data_table.add_row(data[i])
-
 ############ Functions for exporting pdfs using weasyprint library ########
-
 
 def html2pdf(filepath, dstats, listvstats):
     my_path = os.path.abspath(os.path.dirname(__file__))
