01fd5de02783c74579c62ae3512ee7e00d6f6f21
iosif <iosifspart@yahoo.gr>
2021-01-12 19:19:40 +0200

    changes in build scripts because of dh-virtualenv is not included in ubuntu 20.04 repo

--- build.sh
+++ build.sh
@@ -24,7 +24,8 @@ sed -r -e s/#UUID#/$(< /proc/sys/kernel/random/uuid)/g \
     -e s/#DIST_ID#/$dist_id/g -e s/#CODENAME#/$codename/g \
     -e s/#NODEREPO#/$NODEREPO/g -e s/#PYPI#/$pypi_name/g -e s/#PKGNAME#/$pkgname/g \
     <Dockerfile.build >$staging_dir/Dockerfile
-
+cp dh-virtualenv-build-deps_1.2.2-1_all.deb $staging_dir/dh-virtualenv-build-deps_1.2.2-1_all.deb
+cp dh-virtualenv_1.2.2-1~bionic_all.deb $staging_dir/dh-virtualenv_1.2.2-1~bionic_all.deb
 # Build in Docker container, save results, and show package info
 docker build --tag $tag "$@" $staging_dir
 docker run --rm $tag tar -C /dpkg -c . | tar -C build -xv
--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
-python3 -m vevn venv/qctool
 pip install -r requirements.txt
 pip install .
