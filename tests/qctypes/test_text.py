1110e7c9a3d8c470fe087889d5ecb0f533495aaf
iosif <iosifspart@yahoo.gr>
2021-04-15 12:04:38 +0300

    corrected qctype test_text, added text.csv in the test datasets

--- tests/qctypes/test_text.py
+++ tests/qctypes/test_text.py
@@ -3,13 +3,18 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+import os
 import pytest
 import csv
+from pathlib import Path
+
 from mipqctool.model import qctypes
 from mipqctool.config import ERROR
 
 # Tests
+TESTS_BASE_DIR = Path(__file__).resolve().parent.parent
 
+TEST_TEXT_PATH = os.path.join(str(TESTS_BASE_DIR), 'test_datasets', 'text.csv')
 
 @pytest.mark.parametrize('value, result', [
     ('NaN', 'nan'),
@@ -46,11 +51,11 @@ def test_describe_text(pattern, uniques, result):
 
 
 @pytest.mark.parametrize('path, column, result', [
-    ('data/text.csv', 1,
+    (TEST_TEXT_PATH, 1,
      {'top': 'Germany', 'freq': 17, 'unique': 13, 
       'top5': ['Germany', 'Italy', 'Netherlands', 'Denmark', 'Belgium'],
       'bottom5': ['Austria', 'Albania', 'Ireland', 'Turkey', 'Spain']}),
-    ('data/text.csv', 2,
+    (TEST_TEXT_PATH, 2,
      {'top': '', 'freq': 100, 'unique': 1,
       'top5': [''], 'bottom5': ['']})
 ])
